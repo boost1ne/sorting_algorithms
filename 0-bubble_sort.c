3975f985c3d0347810a6963b09f2de4e28cef7d3
boost1ne <boustane.chaimaa@gmail.com>
2024-04-29 11:49:02 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,16 +12,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	long unsigned int i, j, tmp;
+	unsigned long int i, j, k,  tmp;
+
+	if (size <= 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
+		{
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 			}
+		}
+
+		for (k = 0; k < size; k++)
+			printf("%d ", array[k]);
 	}
 }
