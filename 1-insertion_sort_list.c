b32c5d815793a0d7bfcf31576189775f26fba118
boost1ne <boustane.chaimaa@gmail.com>
2024-04-29 15:01:03 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,50 @@
 #include "sort.h"
+
+/**
+ * insertion_sort_list - Inserts a new node with a given data value
+ * into a sorted doubly linked list at its correct position
+ *
+ * @list: List to be sorted
+ **/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *next_node, *new_head;
+
+	if (list == NULL || *list == NULL)
+	{
+		return;
+	}
+
+	new_head = (*list);
+	current = new_head->next;
+
+	while (current)
+	{
+		next_node = current->next;
+
+		while (current->prev && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next)
+			{
+				current->next->prev = current->prev;
+			}
+
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+
+			if (current->prev)
+			{
+				current->prev->next = current;
+			}
+			else
+			{
+				new_head = current;
+			}
+			print_list(new_head);
+		}
+		current = next_node;
+	}
+	*list = new_head;
+}
