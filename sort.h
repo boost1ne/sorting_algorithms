f1076d3e997bcde86b3396d15830f296c3402994
boost1ne <boustane.chaimaa@gmail.com>
2024-04-30 15:38:12 -0700

    quick

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quickSort(int *array, int low, int high, size_t size);
 listint_t *create_listint(const int *array, size_t size);
 
 #endif
