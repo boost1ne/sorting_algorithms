5b6b6e3e24dfd0451dd7f1dd81bff1deef8cdb5b
boost1ne <boustane.chaimaa@gmail.com>
2024-04-30 09:58:40 -0700

    start

---
+++ 2-selection_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+
+/*****/
+
+void selection_sort(int *array, size_t size)
+{
+
+}
--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 listint_t *create_listint(const int *array, size_t size);
 
 #endif
